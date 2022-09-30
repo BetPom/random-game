db944863c65707d44d47ba2d3f6cb2e8a7b038f6
BetPom <b.pomalecka8@gmail.com>
2022-09-30 22:54:24 +0200

    nowa wiadomosc

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String[] gameConsoles={"PlayStation 5", "PlayStatin 5 Digital",
-        "Xbox","xsxs", "sfd", "afsvx", "123"};
+        "Xbox","xsxs", "sfd", "afsvx", "123", "456"};
                 double lucky=Math.random();
                 lucky *=6;
                 int indexLucky=(int)lucky;
