45fca457eed30b3208e4f2ac5f593afa25a8164a
BetPom <b.pomalecka8@gmail.com>
2022-09-30 22:42:26 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] gameConsoles={"PlayStation 5", "PlayStatin 5 Digital",
+        "Xbox","xsxs", "sfd", "afsvx"};
+                double lucky=Math.random();
+                lucky *=6;
+                int indexLucky=(int)lucky;
+        System.out.printf(gameConsoles[indexLucky]);
+
+    }
+}
